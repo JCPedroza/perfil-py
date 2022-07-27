0151bd462e27db179881cd8b441e5222b48e2cb5
JCPedroza <nemesis4go10@hotmail.com>
2022-07-27 06:45:54 -0500

    feat: add setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="pyexample",
+    version="0.1.0",
+    description="A example Python package",
+    url="https://github.com/shuds13/pyexample",
+    author="JCPedroza",
+    author_email="<nemesis4go10@hotmail.com>",
+    license="MIT",
+    packages=["pyprofile"],
+    install_requires=[
+        "setuptools",
+    ],
+    classifiers=[
+        "License :: MIT",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
