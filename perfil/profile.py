1411aa824d65fe60fb3943faf6ad7e6628f18786
JCPedroza <nemesis4go10@hotmail.com>
2022-07-27 12:25:34 -0500

    feat: separate argument lists

--- perfil/profile.py
+++ perfil/profile.py
@@ -25,18 +25,18 @@ class Timers:
         stop_time = self.__funs[0]()
         stop_count = self.__funs[1]()
         self.__totals[0] += stop_time - self.__buffer[0]
-        self.__totals[1] += stop_count - self._buffer[1]
+        self.__totals[1] += stop_count - self.__buffer[1]
 
 
 class FunctionInfo:
     def __init__(self, *, fun: Callable, args: tuple):
         self.fun = fun
-        self.args = list(args)
+        self.args = args
 
 
 class SubjectInfo:
     def __init__(self, *, shuffle: bool, reps: int, label: str):
-        if (reps < 1):
+        if reps < 1:
             raise ValueError(f"reps < 1: {reps}")
 
         self.reps = reps
@@ -49,25 +49,27 @@ class TimeSubject:
         self.__funinfo = funinfo
         self.__subinfo = subinfo
         self.__timers = Timers()
+        self.__shuffable_args = list(funinfo.args)
 
     def __str__(self):
-        head = f"\nTimeSubject\n{self.label}\n{self.name=}\n{self.args=}"
-        mid = f"\n{self.reps=}\nself.timers={str(self.__timers)}\n"
-        return f"{head}{mid}"
+        head = f"\nTimeSubject\n{self.label=}\n{self.name=}\n{self.reps=}"
+        mid = f"\n{self.shuffle=}\nself.timers={str(self.__timers)}"
+        bot = f"\n{self.args=}\n{self.shuffled_args=}"
+        return f"{head}{mid}{bot}"
 
     def __shuffle_args(self):
-        shuffle(self.__funinfo.args)
+        shuffle(self.__shuffable_args)
 
-    def __time_fun_call(self, arg_index):
+    def __time_fun_call(self, arg):
         self.__timers.start()
-        self.__funinfo.fun(self.__funinfo.args[arg_index])
+        self.__funinfo.fun(arg)
         self.__timers.stop()
 
     def __time_fun_arglist(self):
         if self.shuffle:
             self.__shuffle_args()
 
-        for arg in self.__funinfo.args:
+        for arg in self.__shuffable_args:
             self.__time_fun_call(arg)
 
     def profile(self):
@@ -83,7 +85,11 @@ class TimeSubject:
 
     @property
     def args(self) -> tuple:
-        return tuple(self.__funinfo.args)
+        return self.__funinfo.args
+
+    @property
+    def shuffled_args(self) -> tuple:
+        return tuple(self.__shuffable_args)
 
     @property
     def reps(self) -> int:
--- perfil/test_profile.py
+++ perfil/test_profile.py
@@ -4,7 +4,7 @@ from perfil.profile import TimeSubject, FunctionInfo, SubjectInfo
 def test_subjects():
     time_subject = TimeSubject(
         FunctionInfo(fun=lambda x: x, args=(0, 5, 9, -3)),
-        SubjectInfo(label="test label", shuffle=True, reps=10)
+        SubjectInfo(label="test label", shuffle=True, reps=10),
     )
 
     assert time_subject.label == "test label"
