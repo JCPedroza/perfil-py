ee474b8abbecdd1d87d5996fa0402362a340fe9f
JCPedroza <nemesis4go10@hotmail.com>
2022-07-28 23:18:38 -0500

    feat: implement PRofileCollection

--- perfil/profiling.py
+++ perfil/profiling.py
@@ -1,63 +1,99 @@
-from random import shuffle as shuffle_array
-from perfil.timing import Timers
-from perfil.info import FunctionInfo, SubjectInfo
+from random import shuffle as shuffle_list
+from typing import Callable
+from timing import Timer
 
 
-class TimeSubject:
-    def __init__(self, funinfo: FunctionInfo, subinfo: SubjectInfo):
-        self.__funinfo = funinfo
-        self.__subinfo = subinfo
-        self.__timers = Timers()
-        self.__shuffable_args = list(funinfo.args)
+class Profile:
+    def __init__(self, fun: Callable, args: tuple, reps: int, shuffle: bool):
+        self.__fun = fun
+        self.__args = args
+        self.__shuffle = shuffle
+        self.__reps = reps
+        self.__timer = Timer()
+        self.__shufflable_args = list(args)
+        self.__verbose = True
 
     def __str__(self):
-        head = f"\nTimeSubject\n{self.label=}\n{self.name=}\n{self.reps=}"
-        mid = f"\n{self.shuffle=}\nself.timers={str(self.__timers)}"
-        bot = f"\n{self.args=}\n{self.shuffled_args=}"
+        head = f"\n{self.name=}\n{self.reps=}\n{self.verbose=}"
+        mid = f"\n{self.shuffle=}\n{self.args=}\n{self.shufflable_args=}"
+        bot = f"\nself.timers={str(self.__timer)}"
+
         return f"{head}{mid}{bot}"
 
     def __shuffle_args(self):
-        shuffle_array(self.__shuffable_args)
+        shuffle_list(self.__shufflable_args)
 
-    def __time_fun_call(self, arg):
-        self.__timers.start()
-        self.__funinfo.fun(arg)
-        self.__timers.stop()
+    def __time_fun_call(self, *args):
+        if self.__verbose is True:
+            print(f"Profiling {self.__fun.__name__}({args})...")
+
+        self.__timer.start()
+        self.__fun(*args)
+        self.__timer.stop()
 
     def __time_fun_arglist(self):
         if self.shuffle:
             self.__shuffle_args()
 
-        for arg in self.__shuffable_args:
-            self.__time_fun_call(arg)
+        for args in self.__shufflable_args:
+            self.__time_fun_call(args)
 
-    def profile(self):
+    def mute(self):
+        self.__verbose = False
+
+    def unmute(self):
+        self.__verbose = True
+
+    def profile(self) -> float:
+        self.__timer.reset()
         for _ in range(self.reps):
             self.__time_fun_arglist()
 
-    def print(self):
-        print(self)
+        return self.__timer.total
 
     @property
     def name(self) -> str:
-        return self.__funinfo.fun.__name__
+        return self.__fun.__name__
 
     @property
     def args(self) -> tuple:
-        return self.__funinfo.args
+        return self.__args
 
     @property
-    def shuffled_args(self) -> tuple:
-        return tuple(self.__shuffable_args)
+    def shufflable_args(self) -> tuple:
+        return tuple(self.__shufflable_args)
 
     @property
     def reps(self) -> int:
-        return self.__subinfo.reps
-
-    @property
-    def label(self) -> str:
-        return self.__subinfo.label
+        return self.__reps
 
     @property
     def shuffle(self) -> bool:
-        return self.__subinfo.shuffle
+        return self.__shuffle
+
+    @property
+    def verbose(self) -> bool:
+        return self.__verbose
+
+
+class ProfileCollection:
+    def __init__(self, profiles: tuple[Profile, ...], shuffle: bool):
+        self.__profiles = profiles
+        self.__shuffle = shuffle
+
+    def __shuffle_profiles(self):
+        shuffle_list(self.__profiles)
+
+    def profile(self) -> tuple[float, ...]:
+        if self.__shuffle:
+            self.__shuffle_profiles()
+
+        return tuple(profile.profile() for profile in self.__profiles)
+
+    @property
+    def profiles(self):
+        return self.__profiles
+
+    @property
+    def shuffle(self):
+        return self.__shuffle
