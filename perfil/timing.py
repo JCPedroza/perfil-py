a699296545d7bd207fff61f44a6c1da56725c634
JCPedroza <nemesis4go10@hotmail.com>
2022-07-28 23:17:00 -0500

    feat: add reset method

--- perfil/timing.py
+++ perfil/timing.py
@@ -1,26 +1,26 @@
 from timeit import default_timer as time
-from time import perf_counter as count
 
 
-class Timers:
+class Timer:
     def __init__(self):
-        self.__funs = (time, count)
-        self.__totals = [0.0, 0.0]
-        self.__buffer = [0.0, 0.0]
+        self.__time = time
+        self.__buffer = 0.0
+        self.__total = 0.0
 
     def __str__(self) -> str:
-        return f"{self.totals}"
+        return f"Timer total: {self.__total}"
 
     @property
-    def totals(self) -> tuple[float, float]:
-        return (self.__totals[0], self.__totals[1])
+    def total(self) -> float:
+        return self.__total
 
     def start(self):
-        self.__buffer[0] = self.__funs[0]()
-        self.__buffer[1] = self.__funs[1]()
+        self.__buffer = self.__time()
 
     def stop(self):
-        stop_time = self.__funs[0]()
-        stop_count = self.__funs[1]()
-        self.__totals[0] += stop_time - self.__buffer[0]
-        self.__totals[1] += stop_count - self.__buffer[1]
+        stop_time = self.__time()
+        self.__total += stop_time - self.__buffer
+
+    def reset(self):
+        self.__buffer = 0.0
+        self.__total = 0.0
