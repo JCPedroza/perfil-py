cdb9fd691ec8ddf388b6f9241f99d25a91c0203b
JCPedroza <nemesis4go10@hotmail.com>
2022-07-30 17:06:44 -0500

    feat: expose classes through one function

--- perfil/profiling.py
+++ perfil/profiling.py
@@ -1,35 +1,76 @@
 from random import shuffle as shuffle_list
-from typing import Callable
+from collections.abc import Sequence
+from dataclasses import dataclass
+from typing import Callable, overload
+
 from timing import Timer
 
 
+@dataclass(frozen=True)
+class ProfileSpec:
+    """Immutable container of profile specifications."""
+
+    fun: Callable
+    args: tuple
+    reps: int
+    shuffle: bool
+
+
+@dataclass(frozen=True)
+class ProfileResult:
+    """Immutable container of profile results."""
+
+    spec: ProfileSpec
+    total: float
+
+
 class Profile:
-    def __init__(self, fun: Callable, args: tuple, reps: int, shuffle: bool):
-        self.__fun = fun
-        self.__args = args
-        self.__shuffle = shuffle
-        self.__reps = reps
+    """Handles function profiling."""
+
+    def __init__(self, spec: ProfileSpec):
+        self.__spec = spec
         self.__timer = Timer()
-        self.__shufflable_args = list(args)
+        self.__shufflable_args = list(spec.args)
         self.__verbose = True
 
     def __str__(self):
         head = f"\n{self.name=}\n{self.reps=}\n{self.verbose=}"
         mid = f"\n{self.shuffle=}\n{self.args=}\n{self.shufflable_args=}"
-        bot = f"\nself.timers={str(self.__timer)}"
+        bot = f"\nself.timer={str(self.__timer)}"
 
         return f"{head}{mid}{bot}"
 
+    def __lt__(self, other):
+        return self.total < other.total
+
+    def __le__(self, other):
+        return self.total <= other.total
+
+    def __eq__(self, other):
+        return self.total == other.total
+
+    def __ne__(self, other):
+        return self.total != other.total
+
+    def __gt__(self, other):
+        return self.total > other.total
+
+    def __ge__(self, other):
+        return self.total >= other.total
+
     def __shuffle_args(self):
-        shuffle_list(self.__shufflable_args)
+        shuffle_list(self.shufflable_args)
 
     def __time_fun_call(self, *args):
-        if self.__verbose is True:
-            print(f"Profiling {self.__fun.__name__}({args})...")
+        if self.__verbose:
+            print(f"Profiling {self.name}{args}...")
 
-        self.__timer.start()
-        self.__fun(*args)
-        self.__timer.stop()
+        fun = self.__spec.fun
+        timer = self.__timer
+
+        timer.start()
+        fun(*args)
+        timer.stop()
 
     def __time_fun_arglist(self):
         if self.shuffle:
@@ -44,51 +85,85 @@ class Profile:
     def unmute(self):
         self.__verbose = True
 
-    def profile(self) -> float:
+    def profile(self) -> ProfileResult:
         self.__timer.reset()
         for _ in range(self.reps):
             self.__time_fun_arglist()
 
+        return ProfileResult(self.__spec, self.__timer.total)
+
+    @property
+    def total(self) -> float:
         return self.__timer.total
 
     @property
     def name(self) -> str:
-        return self.__fun.__name__
+        return self.__spec.fun.__name__
 
     @property
     def args(self) -> tuple:
-        return self.__args
+        return self.__spec.args
 
     @property
-    def shufflable_args(self) -> tuple:
-        return tuple(self.__shufflable_args)
+    def shufflable_args(self) -> list:
+        return self.__shufflable_args[:]
 
     @property
     def reps(self) -> int:
-        return self.__reps
+        return self.__spec.reps
 
     @property
     def shuffle(self) -> bool:
-        return self.__shuffle
+        return self.__spec.shuffle
 
     @property
     def verbose(self) -> bool:
         return self.__verbose
 
 
-class ProfileCollection:
+class ProfileGroup(Sequence[Profile]):
+    """Groups and handles multiple profiles."""
+
     def __init__(self, profiles: tuple[Profile, ...], shuffle: bool):
         self.__profiles = profiles
+        self.__shufflable_profiles = list(profiles)
         self.__shuffle = shuffle
+        super().__init__()
+
+    @overload
+    def __getitem__(self, index: int) -> Profile:
+        ...
+
+    @overload
+    def __getitem__(self, indexes: slice) -> Sequence[Profile]:
+        ...
+
+    def __getitem__(self, index: int | slice) -> Profile | Sequence[Profile]:
+        if isinstance(index, int):
+            if index >= len(self):
+                raise IndexError("Index out of range")
+        return self.__profiles[index]
+
+    def __len__(self) -> int:
+        return len(self.__profiles)
 
     def __shuffle_profiles(self):
-        shuffle_list(self.__profiles)
+        shuffle_list(self.__shufflable_profiles)
 
-    def profile(self) -> tuple[float, ...]:
+    def profile(self) -> tuple[ProfileResult, ...]:
         if self.__shuffle:
             self.__shuffle_profiles()
 
-        return tuple(profile.profile() for profile in self.__profiles)
+        results = (profile.profile() for profile in self.__shufflable_profiles)
+        return tuple(sorted(results, key=lambda a: a.total))
+
+    def mute(self):
+        for profile in self.__profiles:
+            profile.mute()
+
+    def unmute(self):
+        for profile in self.__profiles:
+            profile.unmute()
 
     @property
     def profiles(self):
@@ -97,3 +172,14 @@ class ProfileCollection:
     @property
     def shuffle(self):
         return self.__shuffle
+
+
+def build_profile_group(
+    funs: Sequence[Callable],
+    args: tuple,
+    reps: int,
+    shuffle: bool,
+) -> ProfileGroup:
+    """Create a profile group."""
+    profiles = tuple(Profile(ProfileSpec(fun, args, reps, shuffle)) for fun in funs)
+    return ProfileGroup(profiles, shuffle)
--- perfil/profiling_test.py
+++ perfil/profiling_test.py
@@ -1,14 +1,42 @@
-from profiling import Profile
+from profiling import build_profile_group
 
 
-def my_test_function(my_test_arg):
+def my_first_test_function(my_test_arg):
+    return my_test_arg
+
+
+def my_second_test_function(my_test_arg):
+    return my_test_arg
+
+
+def my_third_test_function(my_test_arg):
     return my_test_arg
 
 
 def test_profiling():
-    time_subject = Profile(my_test_function, (0, 5, 9, -3), 10, True)
+    funs = [my_first_test_function, my_second_test_function, my_third_test_function]
+    args = (-5, -2, 0, 2, 5)
+    reps = 30
+    shuffle = True
+    profiles = build_profile_group(funs, args, reps, shuffle)
 
-    assert time_subject.name == "my_test_function"
-    assert time_subject.reps == 10
-    assert time_subject.shuffle is True
-    assert time_subject.profile() > 0
+    profiles.mute()
+
+    # Profile group must be indexable.
+    assert profiles[0].name == "my_first_test_function"
+    assert profiles[1].name == "my_second_test_function"
+    assert profiles[2].name == "my_third_test_function"
+    assert len(profiles) == 3
+
+    results = profiles.profile()
+
+    # Check that results are in ascending order.
+    for index in range(len(results) - 1):
+        assert results[index].total <= results[index + 1].total
+
+    # Profile group must be iterable
+    for profile in profiles:
+        assert profile.total > 0
+        assert profile.args == args
+        assert profile.reps == reps
+        assert profile.shuffle == shuffle
